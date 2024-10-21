9dd84a1d4f878955815af483b3740be7094438bf
Jack Xiao <jackxiao031104@gmail.com>
2024-10-22 00:37:12 +0100

    update queens tower image for about page

--- image_edit.py
+++ image_edit.py
@@ -1,14 +1,13 @@
 from PIL import Image
 
-# Define original dimensions (A, B) and new dimensions (C, D)
-A, B = 693, 462  # Example original dimensions (replace with your values)
-C, D = 1900, 870 # Example new dimensions (replace with your values)
+
+C, D = 1900, 870
 
 # Open the original image
-img = Image.open("img/Queens-Tower-at-night.jpeg")  # Replace with your image file path
+img = Image.open("img/Queens-tower.png")  # Replace with your image file path
 
 img_resized = img.resize((C, D), Image.Resampling.LANCZOS)
 
 # Save or show the resized image
-img_resized.save("img/Queens-Tower-at-night-resized.jpeg")  # You can change the filename as needed
+img_resized.save("img/Queens-tower-resized.png")  # You can change the filename as needed
 img_resized.show()
