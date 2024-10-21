efddb738aff09786419530d62733390165fcbaac
Jack Xiao <jackxiao031104@gmail.com>
2024-10-22 00:49:21 +0100

    update about page image to imperial front door

--- image_edit.py
+++ image_edit.py
@@ -4,10 +4,10 @@ from PIL import Image
 C, D = 1900, 870
 
 # Open the original image
-img = Image.open("img/Queens-tower.png")  # Replace with your image file path
+img = Image.open("img/imperial.png")  # Replace with your image file path
 
 img_resized = img.resize((C, D), Image.Resampling.LANCZOS)
 
 # Save or show the resized image
-img_resized.save("img/Queens-tower-resized.png")  # You can change the filename as needed
+img_resized.save("img/imperial-resized.png")  # You can change the filename as needed
 img_resized.show()
